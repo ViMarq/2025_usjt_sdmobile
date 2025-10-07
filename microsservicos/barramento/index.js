ab5f418220f888b1974280b51872976a27c770a5
Vitoria <vimarquini@gmail.com>
2025-10-06 21:55:30 -0300

    learn(ap01-mss): adiciona barramento de eventos

---
+++ microsservicos/barramento/index.js
@@ -0,0 +1,20 @@
+import express from 'express'
+import axios from 'axios'
+const app = express()
+
+//middleware
+app.use(express.json())
+
+//POST /eventos
+app.post('/eventos', (req, res) => {
+    // const evento = req.body
+    const { body: evento } = req
+    console.log(evento)
+    //disparar o evento para os microsserviços
+    axios.post('http://localhost:4000/eventos', evento)
+    axios.post('http://localhost:5000/eventos', evento)
+    res.end()
+})
+
+const port = 10000
+app.listen(port, () => console.log(`Barramento. Porta ${port}`))
--- microsservicos/observacoes/index.js
+++ microsservicos/observacoes/index.js
@@ -1,13 +1,13 @@
 const express = require('express')
 const app = express()
+const { v4: uuidv4 } = require('uuid')
 app.use(express.json())
+
 /*
 {
     1: [
-        {id: 1, texto: 'comprar açúcar' idLembrete: 1}
-        ],
-    2: [
-    {id: 1, texto: 'pagar conta de luz' idLembrete: 2}
+        {id: 1000, texto: 'comprar açúcar' idLembrete: 1},
+        {id: 1001, texto: 'pagar conta de luz' idLembrete: 2}
     ]
 }
 */
@@ -15,15 +15,24 @@ app.use(express.json())
 const baseObservacoes = {}
 //POST /lembretes/1/observacoes
 app.post('/lembretes/:id/observacoes', (req, res) => {
-
+    const idObs = uuidv4()
+    const { texto } = req.body
+    const observacao = {
+        id: idObs,
+        texto: texto
+    }
+    const observacoes = baseObservacoes[req.params.id] || []
+    observacoes.push(observacao)
+    baseObservacoes[req.params.id] = observacoes
+    res.status(201).json(observacao) // quer dizer que um recurso foi criado. é uma forma mais específica de dizer que deu certo
 })
 
 
 
 //GET
 app.get('/lembretes/:id/observacoes', (req, res) => {
-
+    res.json(baseObservacoes[req.params.id])
 })
 
 const port = 5000
-app.listen(port,() => console.log(`Observações. Porta ${port}.`))
+app.listen(port, console.log(`Observações. Porta ${port}.`))
