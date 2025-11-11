ecf051cf6cd2f6fe180dba67846932dfdf9ee627
Vitoria <vimarquini@gmail.com>
2025-11-10 21:43:41 -0300

    learn(ap01-mss): lida com o codigo 4.3.26
    retomar usando o mapa de funcoes, que já foi implementado

---
+++ microsservicos/classificacao/index.js
@@ -0,0 +1,21 @@
+const axios = require('axios')
+const express = require('express')
+const app = express()
+app.use(express.json())
+
+const palavraChave = ['importante']
+const funcoes = {
+    ObservacaoCriada: (observacao) => {
+        observacao.status = observacao.texto.includes(palavraChave) ? 'Importante' : 'Comum'
+        axios.post('http:localhost:10000/eventos', {
+            tipo: 'ObservacaoClassificada',
+            dados: observacao
+        })   
+    }
+}
+app.post('/eventos', (req, res) => {
+
+})
+
+const port = 7000
+app.listen(port, console.log(`Classificação OK. Porta ${port}`))
